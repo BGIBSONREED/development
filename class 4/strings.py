b8cb08f035480ab7c1a074162464a0ad199e205f
brenetta reed <bgibsonreed@gmail.com>
2024-02-26 18:44:19 -0500

    working on strings

---
+++ class 4/strings.py
@@ -0,0 +1,19 @@
+# operator_code = 'A987'
+# part_id = '49Be'
+# item_number = operator_code + part_id
+# print(item_number)
+
+# first_name = 'Brenetta'
+# last_name = 'Reed'
+# full_name = (first_name + ' ' + last_name)
+# print(full_name)
+
+# '''multiplication'''
+# greeting = 'hip hip horray ' *3
+# print(greeting)
+
+# favorite_color = 'blue' *5
+# print(favorite_color)
+
+
+# reference vs value equality == vs is 
