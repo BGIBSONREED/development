83de987589d3bec0cd756ea09be0bc7695482a67
brenetta reed <bgibsonreed@gmail.com>
2024-03-04 18:44:39 -0500

    conditions

--- class 5/assignment2.py
+++ class 5/assignment2.py
@@ -25,14 +25,28 @@ print('Test 1: Does the email have a "." at the third-to-last index?', test_1)
 # Test 2: It has exactly one "@" symbol, at the fifth-to-last index or earlier, email cannot be @.com
 test_2 = ('@' in email[-6:0:-1])
 print('Test 2: Is there a @ symbol in your email? ', test_2)
+# teacher method
+# test_2 = (email[-6::-1])
+# print(test_2)
 
 # Test 3: There is at least one character before the "@" symbol
 test_3 = ('d' in email)
 print('Test 3: Do you have a least one character before the "@" symbol', test_3)
+# teacher method
+# test_3 = (email.index('@') > 0)
+# print(test_3)
 
 # Test 4: It doesn’t have any spaces (doesn’t contain " ") (neta note full string no spaces)
 test_4 = ('' in email)
 print('Test 4: There are no spaces in your email?', test_4)
+# test_char = " "
+# test_4 = (test_char not in email)
+# print(test_4)
+
 
 #Final Test with and Keyword
 print('Final Test: Was your answer to Test 1 and 2 True?', test_1 and test_2)
+
+# teacher method
+# final_results =  test_1 and test_2 and test_3 and test_4
+# print('it this email valid', final_result)
